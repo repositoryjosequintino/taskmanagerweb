d1f64f340400a6ba964d3831be3a7d82263e6a0f
José Quintinno <josequintino@hotmail.com.br>
2025-01-18 14:09:52 -0300

    feature: Implementar Componente de Header

---
+++ application/components/header/header.component.js
@@ -0,0 +1,4 @@
+angular.module("taskManagerApplicationModule").component("applicationHeader", {
+    templateUrl: "./application/components/header/header.component.html",
+    controller: function () {}
+});
